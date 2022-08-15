7722aedee3da99126899106361b2862b7888a58c
thanhnhat420001639 <106805933+thanhnhat420001639@users.noreply.github.com>
2022-08-15 11:35:39 +0700

    CRUD Comment

---
+++ tgdd/src/main/java/hcmute/edu/tgdd/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package hcmute.edu.tgdd.repository;
+
+import hcmute.edu.tgdd.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+  List<Comment> findAllByProductId(Integer productId);
+
+}
