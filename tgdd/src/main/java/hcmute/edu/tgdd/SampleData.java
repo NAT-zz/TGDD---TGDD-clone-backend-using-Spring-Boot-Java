219f235ba69ee3f414dfeb7115c28149709f03b6
GiaHung5121 <truongtrangiahung@gmail.com>
2022-08-15 20:57:34 +0700

    no message

--- tgdd/src/main/java/hcmute/edu/tgdd/SampleData.java
+++ tgdd/src/main/java/hcmute/edu/tgdd/SampleData.java
@@ -1,4 +1,12 @@
 package hcmute.edu.tgdd;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SampleData {
+    private static final Logger logger = LoggerFactory.getLogger(SampleData.class);
+
 }
